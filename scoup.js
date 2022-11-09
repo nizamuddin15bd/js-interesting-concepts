d728c1055d24aae5f4766d18b263d6fb706d8638
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-09 17:21:49 +0600

    scoup.js

---
+++ scoup.js
@@ -0,0 +1,22 @@
+const favNum = 27;
+
+function add(first, second) {
+    // console.log(mood); //Hositing
+    const result = first + second;
+    console.log(result);
+    if (result > 9) {
+        let mood = 'happy';
+        mood = 'cranky';
+
+    }
+
+    return result;
+}
+const sum = add(11, 35);
+// console.log(mood);
+
+
+for (let i = 0; i < 10; i++) {
+
+}
+console.log(i)
